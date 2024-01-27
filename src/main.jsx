0d8fa1104f2a7e30978cd8e7f09c787fb8f73467
seba ghnem <s.sseba96@gmail.com>
2024-01-27 21:48:08 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter >
+  <BrowserRouter basename='/ST-L2-Task6'>
     <App />
   </BrowserRouter>,
 )
